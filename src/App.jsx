b8ef1c6abc1bb9e010aaa8d37bd16336b2a299c0
junaidrazaq <junaid.razaq@hotmail.com>
2022-07-05 19:16:54 +0100

    added_assets_components_folders_and_added_header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./components/header";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>Welcome to my portfolio!</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
---
+++ src/components/header/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./header.css";
+import CV from "../../assets/JunaidCV.docx";
+
+const Header = () => {
+  // FN: Download CV Button
+  const _onDownload = () => {
+    const link = document.createElement("a");
+    link.download = `JunaidCV`;
+    link.href = CV;
+    link.click();
+  };
+
+  // FN: Get In Touch Button
+  const _onContact = () => {
+    window.open("mailto:junaid.razaq@hotmail.com");
+  };
+
+  // RENDER
+  return (
+    <div className="container header_container">
+      <h1> Hello, I'm Junaid Razaq</h1>
+
+      <div className="header_paragraph">
+        <h2>
+          Take a look at some of the projects I have worked on over the last
+          couple of years
+        </h2>
+      </div>
+
+      <div className="header_job_title">
+        <h3 className="light">Javascript Developer</h3>
+      </div>
+
+      <div className="header_buttons">
+        <button onClick={() => _onDownload()}>Download CV</button>
+        <button onClick={() => _onContact()}>Get in touch</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

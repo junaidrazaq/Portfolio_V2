1d3d18130466e6926c37ba7143bc18d72c8af655
junaidrazaq <junaid.razaq@hotmail.com>
2022-07-05 16:19:04 +0100

    reorganised_folder_structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <h1>Welcome to my portfolio!</h1>
     </div>
   );
-}
+};
 
 export default App;

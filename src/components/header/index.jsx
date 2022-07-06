efad911bc1c90e27f77abec4f6dc1de619431e83
junaidrazaq <junaid.razaq@hotmail.com>
2022-07-06 23:17:19 +0100

    links_added_and_buttons_hovers_configd

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./header.css";
 import CV from "../../assets/JunaidCV.docx";
+import { FaLinkedin, FaGithub, FaGithubSquare } from "react-icons/fa";
 
 const Header = () => {
+  // :State
+  const linkedInLink = "uk.linkedin.com/in/junaid-razaq-2714ab129";
+  const githubWorkLink = "github.com/junaidrazaq";
+  const githubCasualLink = "github.com/junaidrazaqnearu";
+
   // FN: Download CV Button
   const _onDownload = () => {
     const link = document.createElement("a");
@@ -16,10 +22,38 @@ const Header = () => {
     window.open("mailto:junaid.razaq@hotmail.com");
   };
 
+  // FN: Links Buttons
+  const _handleLinkPress = (link) => {
+    window.open(`https://${link}`);
+  };
+
   // RENDER
   return (
     <div className="container header_container">
       <h1> Junaid Razaq Porfolio</h1>
+
+      <div className="header_social_media">
+        <div // Github Work Link
+          onClick={() => _handleLinkPress(githubCasualLink)}
+          className="header_links_button"
+        >
+          <FaGithubSquare size={25} />
+        </div>
+
+        <div // LinkedIn Link
+          className="header_links_button"
+          onClick={() => _handleLinkPress(linkedInLink)}
+        >
+          <FaLinkedin size={25} />
+        </div>
+
+        <div // Github Casual Link
+          onClick={() => _handleLinkPress(githubWorkLink)}
+          className="header_links_button"
+        >
+          <FaGithub size={23} />
+        </div>
+      </div>
 
       <div>
         <h2>
